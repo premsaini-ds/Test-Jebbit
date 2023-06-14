7402348a89e35c7646402af93d60b9d9c6b4608e
premkumarsaini <jhandu.saini@dotsquares.com>
2023-06-14 16:01:45 +0530

    first commit

---
+++ functions/http/api/jebbit-intigration.ts
@@ -0,0 +1,94 @@
+export default async function jebbitIntigration():Promise<returnValue> {
+    
+        console.log("date","Calling API");
+
+        return {
+          body: "Calling API",
+          headers: {},
+          statusCode: 200
+        };
+}
+
+
+
+
+interface functionArgument {
+  /** Object containing each query parameter as  */
+  queryParams: { [key: string]: string},
+
+  /** Internal ID of the site branch. */
+  pathParams: { [key: string]: string | RequestInfo | URL},
+
+  /** Site object containing all deploy-related information. */
+  site: Site
+}
+
+interface Site {
+  /** Internal ID of the site branch. */
+  branchId: string,
+
+  /** Internal ID of the Yext account. */
+  businessId: string,
+
+  /** Display name of the Yext account. */
+  businessName: string,
+
+  /** The Git commit hash associated with the deploy. */
+  commitHash: string,
+
+  /** The Git commit message associated with the deploy. */
+  commitMessage: string,
+
+  /** Internal ID for the deploy. */
+  deployId: string,
+
+  /** The "base URL" used for reverse proxying, as specified in serving.json. */
+  displayUrlPrefix: string,
+
+  /** Environment in which a request is invoked. */
+  invocationContext: 'local' | 'preview' | 'staging' | 'production' | null;
+
+  /** External ID of the Yext account. */
+  partnerId: string,
+
+  /** URL of the deploy in the Yext platform. */
+  platformUrl: string,
+
+  /** URL of preview domain associated with the deploy. */
+  previewDomain: string,
+
+  /** URL of production domain associated with the deploy. */
+  productionDomain: string,
+
+  /** Name of the GitHub branch associated with the deploy. */
+  repoBranchName: string,
+
+  /** URL of the GitHub branch associated with the site. */
+  repoBranchUrl: string,
+
+  /** URL of the GitHub repo associated with the site. */
+  repoUrl: string,
+
+  /** Internal ID of the site. */
+  siteId: string,
+
+  /** Display name of the site. */
+  siteName: string,
+
+  /** URL of staging domain associated with the deploy. */
+  stagingDomain: string,
+
+  /** Universe of the Yext account. */
+  yextUniverse: "development" | "qa" | "sandbox" | "production" | null;
+}
+
+interface returnValue {
+  /** HTTP response body (refer to MDN Web Docs). */
+  body: string,
+
+  /** HTTP response status code (refer to MDN Web Docs). */
+  statusCode: number,
+
+  /** HTTP response headers (refer to MDN Web Docs).  */
+  headers: object
+}
--- src/templates/404.tsx
+++ src/templates/404.tsx
@@ -48,7 +48,7 @@ const FourOhFour: Template<TemplateRenderProps> = () => {
         <Banner name={"404 - Page not found"} />
         <div className="centered-container">
           <div className="flex justify-center items-center text-2xl bg-gray-200 h-60 rounded-md shadow-md">
-            <p>This page does not exist.</p>
+            <h1>404 Not Found</h1>
           </div>
         </div>
       </PageLayout>
